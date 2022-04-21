4344c13d0e5a9c09e6ec0577a7f937c937443554
giabyte_tech <giabyte_tech@ukr.net>
2022-04-21 11:43:53 +0300

    added database dump

--- users.sql
+++ users.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.33, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.28, for Linux (x86_64)
 --
 -- Host: localhost    Database: marlin_immersion
 -- ------------------------------------------------------
--- Server version	5.7.33-0ubuntu0.18.04.1
+-- Server version	8.0.28-0ubuntu0.20.04.3
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,9 +21,9 @@
 
 DROP TABLE IF EXISTS `statuses`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `statuses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `status` varchar(255) NOT NULL,
   `interpretation` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
@@ -46,9 +46,9 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user_sessions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_sessions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `user_id` varchar(255) NOT NULL,
   `hash` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
@@ -71,9 +71,9 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -84,8 +84,8 @@ CREATE TABLE `users` (
   `vk` varchar(255) DEFAULT NULL,
   `telegram` varchar(255) DEFAULT NULL,
   `instagram` varchar(255) DEFAULT NULL,
-  `is_admin` int(11) DEFAULT '0',
-  `status` int(11) DEFAULT '0',
+  `is_admin` int DEFAULT '0',
+  `status` int DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -96,9 +96,13 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'gigabyte_tech@ukr.net','$2y$10$97kTaO8kfcxCJmzANhhHhueIxkEGrTE9rE9xRbh3zS6sbOMmJtHXK','Petro Zaburko','avatar_b658718e4d0851cc83e192d382cb9a77.png','Gigabyte, Tech engenier','+380966347348','Ukraine, Ternopil, Symonenko st.','petroZaburko','petroZaburko','petrozaburko',1,2),(10,'new_user@gmail.com','$2y$10$wlo7UVInK9Fu086AWSjjYu4mXaUeBlNhm/im6hV4GYtsWnx/w4vwC','Other User','avatar_b40ed0297870682f6e8bf6295fd7bebc.png','Other work, QA','+380966347348','Ukraine','vk2','tee2','insta2',0,0),(12,'new_user2@gmail.com','$2y$10$kQvUl0WU0V4WjoZxmzIXF.B2iIjYCW3SAYSYW4xZlPSPZobZqJyHu','Ivan Ivanov','avatar_4edd20153cb90cb0cd52842646eeacff.png','Marlin Dev','+380961234578','Ukraine','','','',0,1),(13,'new_user3@gmail.com','$2y$10$/pEc3USAPzd.hee9qNdNS.UkLIi5T8uIO5bF3RksP9cb4JRCWOtQ6','Oleg Semanov','avatar_13880d0c094667acb54758c4300550c4.png','General Motors inc.','+380964545712','Canada, Ontario','oleg_test_vk','oleg_test_telegram','oleg_test_instagram',0,2);
+INSERT INTO `users` VALUES (1,'new_user@ukr.net','$2y$10$97kTaO8kfcxCJmzANhhHhueIxkEGrTE9rE9xRbh3zS6sbOMmJtHXK','Petro Zaburko','avatar_4dd2f0f206270d752784c5cebbabd1e3.jpg','Gigabyte, Tech engenier','+380961231233','Ukraine, Ternopil','petroZaburko','petroZaburko','petrozaburko',1,1),(2,'new_user1@gmail.com','$2y$10$wlo7UVInK9Fu086AWSjjYu4mXaUeBlNhm/im6hV4GYtsWnx/w4vwC','Vasil Vasilev','avatar_b40ed0297870682f6e8bf6295fd7bebc.png','Other work, QA','+380966347348','Ukraine','other_vk','other_telegram','other_instagram',0,0),(3,'new_user2@gmail.com','$2y$10$kQvUl0WU0V4WjoZxmzIXF.B2iIjYCW3SAYSYW4xZlPSPZobZqJyHu','Ivan Ivanov','avatar_4edd20153cb90cb0cd52842646eeacff.png','Marlin Dev','+380961234578','Ukraine','ivan_test_vk','ivan_test_telegram','ivan_test_instagram',0,1),(4,'new_user3@gmail.com','$2y$10$/pEc3USAPzd.hee9qNdNS.UkLIi5T8uIO5bF3RksP9cb4JRCWOtQ6','Oleg Semanov','avatar_13880d0c094667acb54758c4300550c4.png','General Motors inc.','+380964545712','Canada, Ontario','oleg_test_vk','oleg_test_telegram','oleg_test_instagram',0,2);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'marlin_immersion'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -109,4 +113,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-11 18:01:48
+-- Dump completed on 2022-04-21 11:14:23
